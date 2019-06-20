c40510b82a6d3c9577f3e1f143cedd2231bebb5a
Eric Storey <estorey@gmail.com>
2019-06-20 19:58:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ class Driver {
   
   yearsExperienceFromBeginningOf(year){
     const endDate = new Date( year, 1, 1 );
-    console.log(endDate)
-    console.log(this.joinDate)
     return parseInt(( endDate - this.joinDate ) / ( 365 * 24 * 60 * 60 * 1000 ));
   }
 }
