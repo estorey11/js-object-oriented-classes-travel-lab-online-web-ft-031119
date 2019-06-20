35133500dd60c62c4e7e8554846f3e66c26e2e8e
Eric Storey <estorey@gmail.com>
2019-06-20 19:18:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+class Driver {
+  
+  constructor (name, joinDate) {
+    this.name=name
+    this.joinDate=joinDate
+  }
+  
+  startDate(){
+    return new Date(this.joinDate)
+  }
+  
+  yearsExperienceFromBeginningOf(year){
+    console.log(this.startDate().year)
+    return year - this.startDate().year
+  }
+}
